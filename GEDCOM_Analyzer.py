6d4ef02850129ba8937e91b65b3dbd3f0a141a6c
chardtron <rick.scarpetti@gmail.com>
2016-02-14 17:10:05 -0500

    P03 Analyzer Update
    Updated GEDCOM_Analyzer for P03 to store individuals and families and
    print names.
    
    Also updated .ged file to remove note at top

--- GEDCOM_Analyzer.py
+++ GEDCOM_Analyzer.py
@@ -1,12 +1,88 @@
+"""
+    SSW 555
+    2016 Spring
+    Team 4
+"""
 
-f = open('My-Family-2-Feb-2016.ged', 'r');
+import gedcom
 
-for line in f:    
-    print(line, end='');
+from gedcom.gedcomline import GEDCOMLine
+from gedcom.individual import Individual
+from gedcom.family import Family
 
-    print("Level: " + line.split()[0]);
-        
-    if (line.split()[1] ==  "INDI" or line.split()[1] ==  "FAM" or line.split()[1] ==  "HEAD" or line.split()[1] ==  "TRLR" or line.split()[1] ==  "NOTE" or line.split()[1] ==  "NAME" or line.split()[1] ==  "SEX" or line.split()[1] ==  "BIRT" or line.split()[1] ==  "DEAT" or line.split()[1] ==  "FAMC" or line.split()[1] ==  "FAMS" or line.split()[1] ==  "MARR" or line.split()[1] ==  "HUSB" or line.split()[1] ==  "WIFE" or line.split()[1] ==  "CHIL" or line.split()[1] ==  "DIV" or line.split()[1] ==  "DATE"):
-        print("Tag: " + line.split()[1], end="\r\n\r\n");
-    else:
-        print("Tag: Invalid tag", end='\r\n\r\n');
+def main():
+
+    fName = input('Enter the file name: ')
+    try:
+        f = open(fName)
+    except:
+        print ('File cannot be opened:', fname)
+        exit()    
+
+    inds = dict()
+    fams = dict()
+    processInd = False
+    processFam = False
+
+    gedcom_lines = [GEDCOMLine(line) for line in f]
+    lineno = 0
+
+    for gedline in gedcom_lines:
+        lineno += 1
+
+        if not gedline.has_valid_tag:
+            #print('No valid tag on line ', lineno, '\n')
+            pass
+        else:
+            tag = gedline.tag
+            payload = gedline.payload
+            if gedline.level == 0:              #If top level record, clear state
+                processInd = False
+                processFam = False
+
+            if processInd:                      #If gathering data on individual
+                if tag == "NAME":               #Check tag and store appropriately
+                    inds[uid].name = payload.replace("/","")
+                elif tag == "SEX":
+                    inds[uid].sex = payload
+                elif tag == "BIRT":
+                    pass
+                elif tag == "DEAT":
+                    pass
+                elif tag == "DATE":
+                    if prevTag == "BIRT":
+                        inds[uid].birthday = payload
+                    elif prevTag == "DEAT":
+                        inds[uid].death_date = payload
+            elif processFam:                    #If gathering data on family
+                if tag == "MARR":               #Check tag and store appropriately
+                    pass
+                elif tag == "HUSB":
+                    fams[uid].husband = payload
+                elif tag == "WIFE":
+                    fams[uid].wife = payload
+                elif tag == "CHIL":
+                    fams[uid].add_child(payload)
+                elif tag == "DIV":
+                    pass
+            elif gedline.tag == "INDI":         #If start of individual record
+                uid = gedline.payload           #Store identifier in dictionary and
+                inds[uid] = Individual()        #prepare for more individual info
+                processInd = True
+                processFam = False
+            elif gedline.tag == "FAM":          #If start of family record
+                uid = gedline.payload           #Store identifier in dictionary and
+                fams[uid] = Family()            #prepare for more family info
+                processFam = True
+                processInd = False
+
+            prevTag = tag    #Save tag to associate date on next gedline
+
+    for key in sorted(inds):
+        print("%s %s" % (key, inds[key].name))
+
+    for key in sorted(fams):
+        print("%s %s %s" % (key, inds[fams[key].husband].name, inds[fams[key].wife].name))
+
+if __name__ == '__main__':
+    main()
