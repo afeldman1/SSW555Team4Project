89a48c71e8f32b414bcf08f05aceabce646a029c
WJPierro <wjpierro@gmail.com>
2016-03-23 22:08:51 -0400

    Add reporter test cases for US07 and US08

--- test_gedreporter.py
+++ test_gedreporter.py
@@ -5,7 +5,10 @@
 """
 
 import glob
+import unittest
 
+import gedcom.parser
+from gedreporter import GedReporter
 
 def perform_gedfile_sanity_check():
     
@@ -13,5 +16,35 @@ def perform_gedfile_sanity_check():
     print(str.join('\n',gedfiles))
     
     
+class GedReporterTest(unittest.TestCase):
+
+
+    def test_US07_less_than_150_years_old(self):
+    
+        (inds, fams) = gedcom.parser.parse_file('datafiles/US07_less_than_150_years_old.ged')
+        reporter = GedReporter(inds, fams)
+        ids = [ind.uid for ind in reporter.less_than_150_years_old()]
+
+        self.assertTrue('@I150@' in ids)
+        self.assertTrue('@I151@' in ids)
+        self.assertTrue('@I152@' in ids)
+        self.assertFalse('@I153@' in ids)
+
+    def test_US08_birth_before_marriage_of_parents(self):
+    
+        (inds, fams) = gedcom.parser.parse_file('datafiles/US08_birth_before_marriage_of_parents.ged')
+        reporter = GedReporter(inds, fams)
+        ids = [(child.uid, when, family.uid)
+                for (child, when, family)
+                in reporter.birth_before_marriage_of_parents()]
+
+        self.assertTrue(('@I14@', 'after divorce', '@F1@') in ids)
+        self.assertTrue(('@I11@', 'before marriage', '@F1@') in ids)
+        self.assertFalse(('@I13@', 'after divorce', '@F1@') in ids)
+        self.assertFalse(('@I12@', 'before marriage', '@F1@') in ids)
+    
+
+    
 if __name__ == '__main__':
-    perform_gedfile_sanity_check()    
+    perform_gedfile_sanity_check()
+    unittest.main()
